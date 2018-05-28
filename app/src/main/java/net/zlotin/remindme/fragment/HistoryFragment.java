dfa69693961a4686e8381f0e24533ce9d0fefb30
DmitryZlotin <dmitryzlotin@gmail.com>
2018-05-28 20:52:51 +0300

    Add listadapter & cardview

--- app/src/main/java/net/zlotin/remindme/fragment/HistoryFragment.java
+++ app/src/main/java/net/zlotin/remindme/fragment/HistoryFragment.java
@@ -4,15 +4,22 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.zlotin.remindme.R;
+import net.zlotin.remindme.adapter.RemindListAdapter;
+import net.zlotin.remindme.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryFragment extends AbstractTabFragment {
 
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_histoty;
 
     public  static HistoryFragment getInstanse(Context context){
         Bundle args = new Bundle();
@@ -29,9 +36,30 @@ public class HistoryFragment extends AbstractTabFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
 
+        RecyclerView rv =view.findViewById(R.id.recycleview);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.setAdapter(new RemindListAdapter(createMockRemindListDataData()));
+
         return view;
     }
 
+    private List<RemindDTO> createMockRemindListDataData(){
+
+        List<RemindDTO> data = new ArrayList<>();
+
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+        data.add(new RemindDTO("Item 7"));
+        data.add(new RemindDTO("Item 8"));
+        data.add(new RemindDTO("Item 9"));
+        data.add(new RemindDTO("Item 10"));
+
+        return data;
+    }
 
     public void setContext(Context context) {
         this.context = context;
